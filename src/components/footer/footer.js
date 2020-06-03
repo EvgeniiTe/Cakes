aa19fbaa30b3fd781537353fe0a3c52ace0c89ae
EvgeniiTe <eugenetelnov904@gmail.com>
2020-06-03 18:02:41 +0300

    static

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
         <footer>
             <div class="header-footer-container footer-container">
                 <div class="logo-small">
-                    <img src="#" alt="logo" >
+                    <img src="#" alt="logo" />
                 </div>
                 <div class="name">
                     <p>Bohemian Cakes</p>
                 </div>
                 <div class="social-links">
-                    <a href="#" class="social-link"><img src="#" alt="instagram"></a>
+                    <a href="#" class="social-link"><img src="#" alt="instagram"/></a>
                 </div>
             </div>
         </footer>
---
+++ src/services/service.js
@@ -0,0 +1,43 @@
+ export default class Service {
+    _cakes = [
+        {
+          id: 1,
+          name: '',
+          priceKg: '',
+        },
+    
+        {
+          id: 2,
+          name: '',
+          priceKg: '',
+        },
+    
+        {
+          id: 3,
+          name: '',
+          priceKg: '',
+        },
+    
+        {
+          id: 4,
+          name: '',
+          priceKg: '',
+        },
+    
+        {
+          id: 5,
+          name: '',
+          priceKg: '',
+        },
+    
+        {
+          id: 6,
+          name: '',
+          priceKg: '',
+        },
+    
+
+    ];
+
+    
+ }
