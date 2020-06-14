645409d5b47138c8fa5ab7b871ebfb9671ccf8f6
EvgeniiTe <eugenetelnov904@gmail.com>
2020-06-14 12:46:33 +0300

    fix: _URLCAKES => API_STUB

--- src/services/service.js
+++ src/services/service.js
@@ -1,25 +1,25 @@
-const _URLCAKES = './cakes.json';
+const API_STUB = './cakes.json';
 
 export const getAllCakes = () => {
-  return fetch(`${_URLCAKES}`)
+  return fetch(`${API_STUB}`)
     .then(res => res.json())
     .then(data => data.cakes);
 };
 
 export const getCake = (id) => {
-  return fetch(`${_URLCAKES}`)
+  return fetch(`${API_STUB}`)
     .then(res => res.json())
     .then(data => data.cakes[id - 1]);
 };
 
 export const getNavMain = () => {
-  return fetch(`${_URLCAKES}`)
+  return fetch(`${API_STUB}`)
     .then(res => res.json())
     .then(data => data.navMain);
 };
 
 export const getHeaderEl = () => {
-  return fetch(`${_URLCAKES}`)
+  return fetch(`${API_STUB}`)
     .then(res => res.json())
     .then(data => data.headerEl);
 };
