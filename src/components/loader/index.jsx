90e26c05fa0cb21a3290cc724cb2e610028854b0
EvgeniiTe <eugenetelnov904@gmail.com>
2020-08-23 23:31:24 +0300

    add: spinner with material-ui

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { CircularProgress } from "@material-ui/core";
+import * as S from "./styled.jsx";
 
 export const Loader = () => {
-  return <div>loading...</div>;
+  return (
+    <S.Center>
+      <CircularProgress color="secondary" />
+    </S.Center>
+  );
 };
