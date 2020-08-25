ca1ae26e16983f1d43272afa503a491042c48b15
EvgeniiTe <eugenetelnov904@gmail.com>
2020-08-25 18:53:43 +0300

    add: static form for configurator

--- src/components/configurator/index.jsx
+++ src/components/configurator/index.jsx
@@ -1,58 +1,128 @@
 import React from "react";
-import {
-  Typography,
-  Grid,
-  Select,
-  FormControl,
-  InputLabel,
-  FormLabel,
-} from "@material-ui/core";
+import { Form, Button, Col, Row } from "react-bootstrap";
 
 export const Configurator = () => {
   return (
     <>
-      <Typography component="h1" variant="h5">
-        Конфигуратор заказа
-      </Typography>
-      <form method="post">
-        <Grid container spacing={1} justify="space-between">
-          <Grid item xs={12}>
-            <FormControl>
-              <FormLabel>Label</FormLabel>
-              <InputLabel disabled>Age</InputLabel>
-              <Select
-                native
-                inputProps={{
-                  name: "age",
-                  id: "age-native-simple",
-                }}
-              >
-                <option aria-label="None" value="" />
-                <option value={10}>Ten</option>
-                <option value={20}>Twenty</option>
-                <option value={30}>Thirty</option>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl>
-              <InputLabel htmlFor="age-native-simple">Age</InputLabel>
-              <Select
-                native
-                inputProps={{
-                  name: "age",
-                  id: "age-native-simple",
-                }}
-              >
-                <option aria-label="None" value="" />
-                <option value={10}>Ten</option>
-                <option value={20}>Twenty</option>
-                <option value={30}>Thirty</option>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
-      </form>
+      <Form>
+        <Form.Label as="legend">Конфигуратор заказа</Form.Label>
+
+        <Form.Group controlId="conf-form-1">
+          <Form.Label>Выберете торт из списка</Form.Label>
+          <Form.Control as="select" custom>
+            <option>Сделать</option>
+            <option>MAP</option>
+            <option>ТОРТОВ</option>
+          </Form.Control>
+          <Form.Text className="text-muted">
+            Вы можете вернуться к фотографиям тортов на главной странице
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="conf-form-2">
+          <Form.Label>Количество килограмм</Form.Label>
+          <Form.Control as="select" custom>
+            <option>Сделать</option>
+            <option>MAP</option>
+            <option>возможных КГ для каждого торта (всех тортов)</option>
+          </Form.Control>
+          <Form.Text className="text-muted">
+            Да, есть технические ограничения по количеству килограмм. Однако, Вы
+            всегда можете заказать капкейки или же несколько тортов.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="conf-form-3">
+          <Form.Label>Количество тортов</Form.Label>
+          <Form.Control as="select" custom>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </Form.Control>
+          <Form.Text className="text-muted">
+            Да, есть технические ограничения по количеству тортов на один заказ.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="conf-form-4">
+          <Form.Label>
+            Вы можете заказать дополнительный декор к торту
+          </Form.Label>
+
+          <Row>
+            <Col sm={4}>
+              <Form.Group controlId="conf-form-4-1">
+                <Form.Label>Ягоды:</Form.Label>
+                <Col>
+                  <Form.Check
+                    type="checkbox"
+                    label="Клубника"
+                    name="Клубника"
+                    id="Клубника"
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Голубика"
+                    name="Голубика"
+                    id="Голубика"
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Вишня"
+                    name="Вишня"
+                    id="Вишня"
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+
+            <Col sm={4}>
+              <Form.Group controlId="conf-form-4-2">
+                <Form.Label>Темперированный шоколад:</Form.Label>
+                <Col>
+                  <Form.Check
+                    type="checkbox"
+                    label="Шарики"
+                    name="Шарики"
+                    id="Шарики"
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Плоские фигурки"
+                    name="Плоские фигурки"
+                    id="Плоские фигурки"
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+
+            <Col sm={4}>
+              <Form.Group controlId="conf-form-4-3">
+                <Form.Label>Надпись на торте:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ограничение 30 символов"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form.Group>
+
+        <Form.Group controlId="conf-form-5">
+          <Form.Label>Получить фото торта на память</Form.Label>
+          <Form.Check
+            type="checkbox"
+            label="Получить"
+            name="Получить"
+            id="Получить"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Добавить в корзину
+        </Button>
+      </Form>
     </>
   );
 };
--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { CircularProgress } from "@material-ui/core";
-import * as S from "./styled.jsx";
 
 export const Loader = () => {
-  return (
-    <S.Center>
-      <CircularProgress color="secondary" />
-    </S.Center>
-  );
+  return <div>Loading</div>;
 };
