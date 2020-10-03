94308bbfc6c62aa041d9ace0d433bd28b6bf817f
EvgeniiTe <eugenetelnov904@gmail.com>
2020-10-03 12:15:31 +0300

    add: OrderInfo text

---
+++ src/components/order-info/styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const OrderInfo = styled.div`
+  background-color: blue;
+  margin: 0;
+`;
--- src/components/pages/ConfiguratorPage.jsx
+++ src/components/pages/ConfiguratorPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import * as S from "./styled";
 
 import { Configurator } from "../configurator";
+import { OrderInfo } from "../order-info";
 
 export const ConfiguratorPage = () => {
   return (
     <main>
       <S.MainContainer>
+        <OrderInfo />
         <Configurator />
       </S.MainContainer>
     </main>
